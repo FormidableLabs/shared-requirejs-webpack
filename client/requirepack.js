c8d6b11f0383e243cb960488d129b380e9d63b77
Ryan Roemer <ryan@loose-bits.com>
2015-09-25 17:31:09 -0400

    Start experiment bridge. Code is broken.

---
+++ client/requirepack.js
@@ -0,0 +1,9 @@
+/**
+ * Bridge Webpack-created DLL to RequireJS shared library.
+ */
+(function () {
+  /*eslint-disable no-console*/
+  console.log("TODO HERE -- Start hacking");
+  /*eslint-enable no-console*/
+
+}());
