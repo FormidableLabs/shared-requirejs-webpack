d724b82289b855272fc00807b65b707c30c869c1
Ryan Roemer <ryan@loose-bits.com>
2015-09-25 23:36:15 -0400

    Clean up lint

--- client/app.js
+++ client/app.js
@@ -7,5 +7,5 @@ define([
     .append($("<h2>Foo</h2>"))
     .append($("<code />").text(foo))
     .append($("<h2>Bar</h2>"))
-    .append($("<code />").text(bar))
+    .append($("<code />").text(bar));
 });
--- requirejs.config.js
+++ requirejs.config.js
@@ -1,3 +1,4 @@
+/*eslint-disable strict*/
 (function () {
   // Hack: Make the config directly importable in Node/Grunt.
   if (typeof require.config === "undefined") {
@@ -11,7 +12,7 @@
 
     paths: {
       client: "./",
-      jquery: "../node_modules/jquery/dist/jquery",
+      jquery: "../node_modules/jquery/dist/jquery"
     }
   });
 }());
